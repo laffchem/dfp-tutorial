c1260685f0ed08f1635c766af0a3d21d0468de9b
= <=>
2025-11-18 13:05:24 -0500

    add host for crostini container

--- core/settings.py
+++ core/settings.py
@@ -31,7 +31,13 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1", "100.115.92.196"]
+ALLOWED_HOSTS = [
+    ".herokuapp.com",
+    "localhost",
+    "127.0.0.1",
+    "100.115.92.196",
+    "100.115.92.196:8001",
+]
 
 
 # Application definition
