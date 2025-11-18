5938d6e2dc74467b8c8d8c94221f7f1576b90481
= <=>
2025-11-18 13:13:13 -0500

    Revert "add host for crostini container"
    This reverts commit c1260685f0ed08f1635c766af0a3d21d0468de9b.

--- core/settings.py
+++ core/settings.py
@@ -31,13 +31,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = [
-    ".herokuapp.com",
-    "localhost",
-    "127.0.0.1",
-    "100.115.92.196",
-    "100.115.92.196:8001",
-]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1", "100.115.92.196"]
 
 
 # Application definition
